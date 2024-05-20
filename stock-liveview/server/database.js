05b79a0cfb5f545b95b70255e02362ea57dd0e2c
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-05-20 08:53:17 +0200

    chore: 🔧 Update MongoDB connection URL and initialization in database.js

--- stock-liveview/server/database.js
+++ stock-liveview/server/database.js
@@ -2,18 +2,29 @@ const { MongoClient } = require("mongodb");
 
 let client;
 
-const initializeMongoDB = async () => {
-  const url =
-    process.env.MONGODB_URL ||
-    "mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/?replicaSet=rs0";
-  client = new MongoClient(url);
-  console.log(`Connecting to MongoDB at ${url}`);
-  await client.connect();
-  console.log("Connected to MongoDB");
+const main = async () => {
+  const uri =
+    "mongodb://127.0.0.1:27017/,127.0.0.1:27018,127.0.0.1:27019/?replicaSet=rs0";
+  client = new MongoClient(uri);
+
+  try {
+    console.log("Connecting to MongoDB");
+    await client.connect();
+    console.log("Connected to MongoDB");
+    await listDatabases(client);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const listDatabases = async (client) => {
+  const databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
 };
 
 const getMongoClient = () => {
   return client;
 };
 
-module.exports = { initializeMongoDB, getMongoClient };
+module.exports = { main, getMongoClient };
