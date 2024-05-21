48417f81fa9fdbcb88d24a0b33132312e34646ab
Markus Bürgi <117123240+mBuergi86@users.noreply.github.com>
2024-05-21 08:41:25 +0200

    feat: ✨ Add Express server setup with database and websocket initialization
    - Set up the Express server to serve static files and handle the homepage route
    
    - Added initialization for the database connection and the websocket server
    
    - Configured the server to start on a port specified in the environment variables or default to port 3000

--- stock-liveview/app.js
+++ stock-liveview/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const { initializeWebsocketServer } = require("./server/websocketserver");
-const { main } = require("./server/database");
+const { initializeDatabase } = require("./server/database");
 
 // Create the express server
 const app = express();
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 // Allowing top-level await
 (async function () {
   // Initialize the database
-  await main().catch(console.error);
+  await initializeDatabase().catch(console.error);
   // Initialize the websocket server
   initializeWebsocketServer(server);
   //start the web server
